e9e127469110fd21d1f7b99fd42c9b45c1ddb513
ayjaml <ayjamalruzi923@gmail.com>
2024-01-21 02:03:21 +0400

    hello world

---
+++ src/git_review02/HelloWorld.java
@@ -0,0 +1,12 @@
+package git_review02;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("hhh");
+
+        System.out.println("fff");
+    }
+
+}
